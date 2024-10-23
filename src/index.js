41e82b642618468440f3673f87c5a3cbfbf728d1
Katya <gloooomrine@gmail.com>
2024-10-24 00:06:41 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+let BRACKETS = new Map();
+
+function createBracket(bracketsConfig){
+  for (let i = 0; i < bracketsConfig.length; i++){
+    BRACKETS.set(bracketsConfig[i][0], bracketsConfig[i][1]);
+  }
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  createBracket(bracketsConfig);
+  let stack = [];
+  for (let i = 0; i < str.length; i++){
+    if (stack.length > 0 && str[i] === BRACKETS.get(str[i]) && str[i] === stack.at(-1)){
+      stack.splice(stack.length - 1, 1);
+    }
+
+    else if (BRACKETS.has(str[i])){
+      stack.push(str[i]);
+    }
+
+    else{
+      if (str[i] === BRACKETS.get(stack[stack.length - 1])){
+          stack.splice(stack.length - 1, 1);
+      }
+      else
+          return false;
+    }
+    
+    if (stack.length === 0 && i === str.length - 1){
+        return true;
+    }
+  }
+
+  if (stack.length != 0)
+    return false;
 }
